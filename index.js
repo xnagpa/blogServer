c5bf9bee9c76a7ee9e4526350f1903bbe58a4c72
Maksim <m.glinka@rambler-co.ru>
2017-02-02 14:17:14 +0300

    Improves pagination

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var cors = require('cors');
 
 var entries = require('./data').entries;
 
+const per_page = 2;
+
 application.use(cors());
 
 application.get('/', function(req, res){
@@ -16,7 +18,7 @@ application.get('/posts/:entry_id', function(req, res){
 });
 
 application.get('/posts/pages/:page', function(req, res){
-   res.json(entries.slice(req.params.page-1, 2));
+   res.json(entries.slice((req.params.page-1)*per_page, (req.params.page)*per_page));
 });
 
 application.listen(3001, function() {
